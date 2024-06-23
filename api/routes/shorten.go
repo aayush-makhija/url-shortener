b35fe45cc2cdd21f4aefe1baa2461ac9c74ef8b9
aayush-makhija <115614982+aayush-makhija@users.noreply.github.com>
2024-06-23 19:28:27 +0530

    Update shorten.go

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -14,31 +14,39 @@ import (
 )
 
 type request struct {
-	URL         string        `json: "url"`
-	customShort string        `json: "short"`
-	expiry      time.Duration `json: "expiry"`
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
 }
 
 type response struct {
-	URL             string        `json: "url"`
-	customShort     string        `json: "short"`
-	expiry          time.Duration `json: "expiry"`
-	xRateRemaining  int           `json: "rate_limit"`
-	xRateLimitReset time.Duration `json: "rate_limit_reset"`
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  int           `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
 }
 
+// ShortenURL ...
 func ShortenURL(c *fiber.Ctx) error {
+	// check for the incoming request body
 	body := new(request)
 	if err := c.BodyParser(&body); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error:": "cannot parse JSON"})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "cannot parse JSON",
+		})
 	}
 
-	//implement rate limiting
+	// implement rate limiting
+	// everytime a user queries, check if the IP is already in database,
+	// if yes, decrement the calls remaining by one, else add the IP to database
+	// with expiry of `30mins`. So in this case the user will be able to send 10
+	// requests every 30 minutes
 	r2 := database.CreateClient(1)
 	defer r2.Close()
 	val, err := r2.Get(database.Ctx, c.IP()).Result()
 	if err == redis.Nil {
-		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err() //change the rate_limit_reset here, change `30` to your number
 	} else {
 		val, _ = r2.Get(database.Ctx, c.IP()).Result()
 		valInt, _ := strconv.Atoi(val)
@@ -51,68 +59,71 @@ func ShortenURL(c *fiber.Ctx) error {
 		}
 	}
 
-	//check if the input is an actual URL
-
+	// check if the input is an actual URL
 	if !govalidator.IsURL(body.URL) {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid URL",
+		})
 	}
 
-	//check for domain error
-
+	// check for the domain error
+	// users may abuse the shortener by shorting the domain `localhost:3000` itself
+	// leading to a inifite loop, so don't accept the domain for shortening
 	if !helpers.RemoveDomainError(body.URL) {
-		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "you can't hack the system (: "})
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+			"error": "haha... nice try",
+		})
 	}
 
-	//enforce https, SSL
-
+	// enforce https
+	// all url will be converted to https before storing in database
 	body.URL = helpers.EnforceHTTP(body.URL)
 
+	// check if the user has provided any custom dhort urls
+	// if yes, proceed,
+	// else, create a new short using the first 6 digits of uuid
+	// haven't performed any collision checks on this
+	// you can create one for your own
 	var id string
-
-	if body.customShort == "" {
+	if body.CustomShort == "" {
 		id = uuid.New().String()[:6]
 	} else {
-		id = body.customShort
+		id = body.CustomShort
 	}
 
 	r := database.CreateClient(0)
 	defer r.Close()
 
 	val, _ = r.Get(database.Ctx, id).Result()
+	// check if the user provided short is already in use
 	if val != "" {
 		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
-			"error": "URL custom short is already in use",
+			"error": "URL short already in use",
 		})
 	}
-	if body.expiry == 0 {
-		body.expiry = 24
+	if body.Expiry == 0 {
+		body.Expiry = 24 // default expiry of 24 hours
 	}
-
-	err = r.Set(database.Ctx, id, body.URL, body.expiry*3600*time.Second).Err()
-
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Unable to connect to server.",
+			"error": "Unable to connect to server",
 		})
 	}
-
+	// respond with the url, short, expiry in hours, calls remaining and time to reset
 	resp := response{
 		URL:             body.URL,
-		customShort:     "",
-		expiry:          body.expiry,
-		xRateRemaining:  10,
-		xRateLimitReset: 30,
+		CustomShort:     "",
+		Expiry:          body.Expiry,
+		XRateRemaining:  10,
+		XRateLimitReset: 30,
 	}
-
 	r2.Decr(database.Ctx, c.IP())
-
 	val, _ = r2.Get(database.Ctx, c.IP()).Result()
-	resp.xRateRemaining, _ = strconv.Atoi(val)
-
+	resp.XRateRemaining, _ = strconv.Atoi(val)
 	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
-	resp.xRateLimitReset = ttl / time.Nanosecond / timme.Minute
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
 
-	resp.customShort = os.Getenv("DOMAIN") + "/" + id
-
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
 	return c.Status(fiber.StatusOK).JSON(resp)
 }
